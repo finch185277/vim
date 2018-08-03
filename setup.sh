d22e978f2d6877f3d2f229ada55d89834551461c
finch185277 <59487andy@gmail.com>
2018-08-03 17:23:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-#################################################################
-#   This is a script for auto installing useful tool on Linux   #
-#   Author          > Gavin Lee                                 #
-#   Created Time    > 2016/04/02                                #
-#################################################################
-
-# checking Linux OS
-if which apt-get > /dev/null 2>&1; then
-    yes | sudo apt update
-    yes | sudo apt upgrade
-    app='sudo apt install'
-    yes | $app golang
-else
-    echo "this is not ubuntu"
-fi
-
 # editor
 yes | $app vim
 
